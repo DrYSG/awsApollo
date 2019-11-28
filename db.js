26569d122d2f1b6d38ff581e8e9ef23719c73d57
DrYSG <ygutfreund@draper.com>
2019-11-28 11:24:11 -0500

    working master

--- db.js
+++ db.js
@@ -3,21 +3,50 @@ const { userData } = require('./userData')
 
 const localHost = {
     db: 'm3_db',
+    user: 'postgres',
     host: 'localhost',
     pass: 'yechezkal'
 }
 const awsHost = {
     db: 'apollodb',
+    user: 'postgres',
     host: 'apollodb.cxeokcheapqj.us-east-2.rds.amazonaws.com',
     pass: 'yechezkal'
 }
 
-class DB {
+const azureHost = {
+    db: 'postdb',
+    user: 'postgres@postdb',
+    host: 'postdb.postgres.database.azure.com',
+    pass: 'Yechezkal1'
+}
 
-    async dbSetup(where) {
-        let host = (where == "local") ? localHost : awsHost
+class DB {
+    constructor(where) {
+        this.conn = null
+        this.db = null
+        switch (where) {
+            case 'local':
+                this.conn = localHost; break;
+            case 'aws':
+                this.conn = awsHost; break;
+            case 'azure':
+                this.conn = azureHost; break;
+        }
+    }
+
+    async connect() {
+        if (this.db !== null) return this.db
+        else {
+            await this.open()
+            return this.db
+        }
+    }
+
+    async open() {
+        let host = this.conn
         console.log(`Host: ${JSON.stringify(host)}`)
-        this.db = new Sequelize(host.db, 'postgres', host.pass, {
+        this.db = new Sequelize(host.db, host.user, host.pass, {
             host: host.host,
             dialect: 'postgres',
             logging: false,
@@ -51,21 +80,28 @@ class DB {
     }
 
     async select(id) {
+        await this.connect()
         let who = await this.User.findAll({ where: { id: id } })
+        await this.close()
         return who.get({ plain: true })
     }
 
     async findFirst(name) {
+        await this.connect()
         let me = await this.User.findAll({ where: { firstName: name } })
+        await this.close()
         return me[0].get({ plain: true })
     }
 
     async addUser(user) {
+        await this.connect()
         let me = await this.User.create(user)
+        await this.close()
         return me.get({ plain: true })
     }
 
     async  populate() {
+        await this.connect()
         await this.db.sync({ force: true })
         try {
             await this.User.bulkCreate(userData, { validate: true })
@@ -74,17 +110,21 @@ class DB {
             console.error('failed to create users')
             console.error(err)
         } finally {
+            await this.close()
         }
     }
 
     async findAll() {
+        await this.connect()
         let users = await this.User.findAll({ raw: true })
+        await this.close()
         return users
     }
 
     async close() {
-        this.db.close()
+        await this.db.close()
+        this.db = null
     }
 }
 
-exports.DB = new DB()
+exports.DB = new DB('aws')
--- server.js
+++ server.js
@@ -1,37 +1,23 @@
-const { ApolloServer } = require('apollo-server')
-const { ApolloServer: ApolloServerLambda } = require('apollo-server-lambda')
-const { typeDefs, resolvers, connect } = require('./schema.js')
+const { ApolloServer } = require('apollo-server-lambda')
+const { typeDefs, resolvers } = require('./schema.js')
 
-// The ApolloServer constructor requires two parameters: your schema
-// definition and your set of resolvers.
-
-async function setup(where) {
-  if (where == 'local') {
-    const server = new ApolloServer({ typeDefs, resolvers })
-    let { url } = await server.listen()
-    console.log(`Server ready at ${url}`)
-  } else {
-    const server = new ApolloServerLambda({ 
-      typeDefs, 
-      resolvers,
-      playground: true,
-      introspection: true,
-      cors: {
-        origin: '*',
-        credentials: true,
-      },
-      context: ({ event, context }) => (
-        {
-          headers: event.headers,
-          functionName: context.functionName,
-          event,
-          context
-        })
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+  playground: true,
+  introspection: true,
+  cors: {
+    origin: '*',
+    credentials: true,
+  },
+  context: ({ event, context }) => (
+    {
+      headers: event.headers,
+      functionName: context.functionName,
+      event,
+      context
     })
-    exports.graphqlHandler = server.createHandler()
-  }
-}
+})
+exports.graphqlHandler = server.createHandler()
 
-let location = (process.env.USERNAME == 'ysg4206') ? 'local' : 'aws'
-connect(location, setup)
 
